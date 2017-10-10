75bf13c471137ca4fabf8e4cfd13f24ded4be681
ReddyJK <jreddy2@jhu.edu>
2017-10-10 10:55:00 -0400

    Add existing file

---
+++ 05-10_Make-A-Pull-Request-On-HARK_ReddyJK/cloneHARK_ReddyJK.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ~/JHUassignments/Computational/HARK_ReddyJK
+
+
+echo "# Methods-Assignments" >> README.md
+git init
+
+git add README.md
+
+git remote add origin https://github.com/ReddyJK/HARK.git
+
+git pull https://github.com/ReddyJK/HARK.git
+
---
+++ 05-10_Make-A-Pull-Request-On-HARK_ReddyJK/pullrequest_ReddyJK.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~/JHUassignments/Computational/HARK_ReddyJK/
+
+git add HARK/FashionVictim/FashionVictimModel.py
+git add HARK/FashionVictim/FashionVictimParams.py
+git commit -m "Changes made in fashionmodel parameters"
+
+git push https://github.com/ReddyJK/HARK.git
+
+
